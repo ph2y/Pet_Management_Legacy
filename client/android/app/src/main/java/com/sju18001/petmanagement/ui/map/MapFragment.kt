f03413f6cec098857bde1918ddc900ce0457aef9
Hanbit-Kang <58168528+Hanbit-Kang@users.noreply.github.com>
2021-05-20 22:03:40 +0900

    On MapFragment created, Check if permissions are denied, Request permissions which are denied

--- client/android/app/src/main/java/com/sju18001/petmanagement/ui/map/MapFragment.kt
+++ client/android/app/src/main/java/com/sju18001/petmanagement/ui/map/MapFragment.kt
@@ -1,16 +1,19 @@
 package com.sju18001.petmanagement.ui.map
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.view.isVisible
 
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.sju18001.petmanagement.R
 
+import com.sju18001.petmanagement.R
 import com.sju18001.petmanagement.databinding.FragmentMapBinding
 import net.daum.mf.map.api.MapPoint
 
@@ -25,6 +28,13 @@ class MapFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    // 권한 관련
+    private val REQUEST_CODE = 100
+    private val requiredPermissions = arrayOf(
+        Manifest.permission.INTERNET,
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_COARSE_LOCATION
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -36,8 +46,12 @@ class MapFragment : Fragment() {
 
         _binding = FragmentMapBinding.inflate(inflater, container, false)
         val root: View = binding.root
+        
+        // 권한 확인 및 획득
+        val deniedPermissions = getDeniedPermissions()
+        requestPermissions(deniedPermissions)
 
-        //MavView 초기화
+        // MavView 초기화
         val mapView = MapView(this.activity)
 
         val mapViewContainer = root?.findViewById<ViewGroup>(R.id.map_view)!!
@@ -51,4 +65,22 @@ class MapFragment : Fragment() {
         _binding = null
     }
 
+    private fun getDeniedPermissions():ArrayList<String>{
+        var deniedPermissions = ArrayList<String>()
+
+        for(p in requiredPermissions){
+            if(ContextCompat.checkSelfPermission(requireContext(), p) == PackageManager.PERMISSION_DENIED){
+                deniedPermissions.add(p)
+            }
+        }
+
+        return deniedPermissions
+    }
+
+    private fun requestPermissions(permissions: ArrayList<String>){
+        if(permissions.isNotEmpty()){
+            val array = arrayOfNulls<String>(permissions.size)
+            ActivityCompat.requestPermissions(requireActivity(), permissions.toArray(array), REQUEST_CODE)
+        }
+    }
 }
