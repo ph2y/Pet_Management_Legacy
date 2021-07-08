c65a4b0b22caae686a06ca5feb506da52d38996a
HanJuK <qwerty03081@gmail.com>
2021-07-08 22:23:51 +0900

    created layout for adding a new pet

---
+++ client/android/app/src/main/java/com/sju18001/petmanagement/ui/myPet/AddPetActivity.kt
@@ -0,0 +1,12 @@
+package com.sju18001.petmanagement.ui.myPet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sju18001.petmanagement.R
+
+class AddPetActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_pet)
+    }
+}
