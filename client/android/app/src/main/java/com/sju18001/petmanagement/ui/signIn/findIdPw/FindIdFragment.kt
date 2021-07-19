dcf74e226bc35cd1177f6a8fcb84a2ff508f86b0
Hanbit-Kang <58168528+Hanbit-Kang@users.noreply.github.com>
2021-07-19 22:05:58 +0900

    Fix error occurs for typed edit text when fragment restart in find id pw fragment

--- client/android/app/src/main/java/com/sju18001/petmanagement/ui/signIn/findIdPw/FindIdFragment.kt
+++ client/android/app/src/main/java/com/sju18001/petmanagement/ui/signIn/findIdPw/FindIdFragment.kt
@@ -11,22 +11,21 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import com.sju18001.petmanagement.R
+import com.sju18001.petmanagement.controller.ServerUtil
 import com.sju18001.petmanagement.controller.Util
-import com.sju18001.petmanagement.databinding.FragmentFindIdBinding
-import com.sju18001.petmanagement.restapi.AccountFindUsernameRequestDto
-import com.sju18001.petmanagement.restapi.AccountFindUsernameResponseDto
-import com.sju18001.petmanagement.restapi.RetrofitBuilder
+import com.sju18001.petmanagement.databinding.FragmentFindPwBinding
+import com.sju18001.petmanagement.restapi.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.util.regex.Pattern
 
-class FindIdFragment : Fragment() {
-    private var _binding: FragmentFindIdBinding? = null
+class FindPwFragment : Fragment() {
+    private var _binding: FragmentFindPwBinding? = null
     private val binding get() = _binding!!
 
-    private val INPUT_LENGTH = 1
-    private val EMAIL = 0
-    private val isValidInput: HashMap<Int, Boolean> = HashMap()
+    // 정규식
+    private val patternUsername: Pattern = Pattern.compile("^[a-z0-9]{5,16}$")
 
     private var isViewDestroyed: Boolean = false
 
@@ -35,57 +34,27 @@ class FindIdFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentFindIdBinding.inflate(inflater, container, false)
+        _binding = FragmentFindPwBinding.inflate(inflater, container, false)
 
-        if(savedInstanceState?.getBoolean("is_result_shown") == true){
-            val username = savedInstanceState.getString("result_username")
-            setViewForResult(username)
+        when(savedInstanceState?.getInt("page")){
+            1 -> setViewForCodeInput()
+            2 -> setViewForResult()
+            else -> setViewForEmailInput()
         }
 
         return binding.root
     }
 
-    override fun onStart() {
-        super.onStart()
-
-        // initialize valid input map
-        for(i in 0 until INPUT_LENGTH) { isValidInput[i] = false }
-        checkEmailValidation(binding.emailEditText.text)
-        checkIsValid()
-        setMessageGone()
-
-        // 아이디 찾기 버튼 클릭
-        binding.findIdButton.setOnClickListener{
-            activity?.let { Util().hideKeyboard(it) }
-
-            findUsername(binding.emailEditText.text.toString())
-        }
-
-        // 레이아웃 클릭
-        binding.findIdLayout.setOnClickListener{
-            activity?.let { Util().hideKeyboard(it) }
-        }
-
-        // 이메일 입력란 입력
-        binding.emailEditText.addTextChangedListener(object: TextWatcher {
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                checkEmailValidation(s)
-                checkIsValid()
-            }
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-            override fun afterTextChanged(s: Editable?) {}
-        })
-    }
-
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
 
-        if(binding.resultLayout.visibility == View.VISIBLE){
-            outState.putBoolean("is_result_shown", true)
-            outState.putString("result_username", binding.resultUsername.text.toString())
-        }else{
-            outState.putBoolean("is_result_shown", false)
+        var page: Int = 0
+        if(binding.codeInputLayout.visibility == View.VISIBLE){
+            page = 1
+        }else if(binding.resultLayout.visibility == View.VISIBLE){
+            page = 2
         }
+        outState.putInt("page", page)
     }
 
     override fun onDestroyView() {
@@ -95,94 +64,206 @@ class FindIdFragment : Fragment() {
     }
 
 
-    // * 유효성 검사
-    private fun checkEmailValidation(s: CharSequence?){
+    // 유효성 검사
+    private fun checkEmailValidation(s: CharSequence?) {
         if(Patterns.EMAIL_ADDRESS.matcher(s).matches()) {
-            isValidInput[EMAIL] = true
             binding.emailMessage.visibility = View.GONE
+            binding.emailInputButton.isEnabled = true
         }
         else {
-            isValidInput[EMAIL] = false
             binding.emailMessage.visibility = View.VISIBLE
+            binding.emailInputButton.isEnabled = false
         }
     }
 
-    private fun checkIsValid() {
-        for(i in 0 until INPUT_LENGTH) {
-            if(!isValidInput[i]!!) {
-                // if not valid -> disable button + return
-                binding.findIdButton.isEnabled = false
-                return
+    private fun checkUsernameValidation(s: CharSequence?) {
+        if(patternUsername.matcher(s).matches()) {
+            binding.usernameMessage.visibility = View.GONE
+            binding.codeInputButton.isEnabled = true
+        }
+        else {
+            binding.usernameMessage.visibility = View.VISIBLE
+            binding.codeInputButton.isEnabled = false
+        }
+    }
+
+    // 이메일 입력창
+    private fun setViewForEmailInput(){
+        // 레이아웃 전환
+        binding.emailInputLayout.visibility = View.VISIBLE
+        binding.codeInputLayout.visibility = View.GONE
+        binding.resultLayout.visibility = View.GONE
+
+        // 정규식 검사
+        checkEmailValidation(binding.emailEditText.text)
+        setMessageGone()
+
+        // 이메일 입력란 입력
+        binding.emailEditText.addTextChangedListener(object: TextWatcher {
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                checkEmailValidation(s)
             }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+        // 버튼 클릭
+        binding.emailInputButton.setOnClickListener{
+            activity?.let { Util().hideKeyboard(it) }
+
+            // 인증코드 전송
+            sendAuthCode(binding.emailEditText.text.toString())
         }
 
-        // if all is valid -> enable button
-        binding.findIdButton.isEnabled = true
+        // 레이아웃 클릭
+        binding.emailInputLayout.setOnClickListener{
+            activity?.let { Util().hideKeyboard(it) }
+        }
     }
 
+    // 코드 입력창
+    private fun setViewForCodeInput() {
+        // 레이아웃 전환
+        binding.emailInputLayout.visibility = View.GONE
+        binding.codeInputLayout.visibility = View.VISIBLE
+        binding.resultLayout.visibility = View.GONE
+
+        // 정규식 검사
+        checkUsernameValidation(binding.usernameEditText.text)
+        setMessageGone()
+
+        // 아이디 입력란 입력
+        binding.usernameEditText.addTextChangedListener(object: TextWatcher {
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                checkUsernameValidation(s)
+            }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+        // 버튼 클릭
+        binding.codeInputButton.setOnClickListener{
+            activity?.let { Util().hideKeyboard(it) }
+
+            // 코드 확인
+            findPassword(binding.usernameEditText.text.toString(), binding.codeEditText.text.toString())
+        }
+
+        // 레이아웃 클릭
+        binding.codeInputLayout.setOnClickListener{
+            activity?.let { Util().hideKeyboard(it) }
+        }
+    }
+
+    // 비밀번호 찾기 결과
+    private fun setViewForResult(){
+        // 레이아웃 전환
+        binding.emailInputLayout.visibility = View.GONE
+        binding.codeInputLayout.visibility = View.GONE
+        binding.resultLayout.visibility = View.VISIBLE
+    }
+
+    // 에러 메시지 제거
     private fun setMessageGone() {
         binding.emailMessage.visibility = View.GONE
+        binding.usernameMessage.visibility = View.GONE
+        binding.codeMessage.visibility = View.GONE
     }
 
-
-    // 아이디 찾기
-    private fun findUsername(email: String){
-        val reqBody = AccountFindUsernameRequestDto(email)
-        val call = RetrofitBuilder.getServerApi().findUsernameRequest(reqBody)
+    // 코드 전송
+    private fun sendAuthCode(email: String){
+        val accountSendAuthCodeRequestDto = AccountSendAuthCodeRequestDto(email)
 
         // 버튼 로딩 상태
-        setButtonLoading(true)
+        setEmailInputButtonLoading(true)
 
-        call.enqueue(object: Callback<AccountFindUsernameResponseDto> {
-            override fun onResponse(
-                call: Call<AccountFindUsernameResponseDto>,
-                response: Response<AccountFindUsernameResponseDto>
-            ) {
+        ServerUtil.sendAuthCode(accountSendAuthCodeRequestDto,
+            { response ->
                 if(!isViewDestroyed){
                     // 버튼 로딩 상태 해제
-                    setButtonLoading(false)
+                    setEmailInputButtonLoading(false)
 
                     if(response.isSuccessful){
-                        response.body()?.let{
-                            setViewForResult(it.username)
-                        }
+                        // 코드 입력
+                        setViewForCodeInput()
                     }else{
-                        Toast.makeText(context, "해당 이메일을 찾을 수 없습니다.", Toast.LENGTH_LONG).show()
+                        // 어떤 이메일이든 코드 전송은 하기 때문에, 보통 실패할 수 없다.
+                        Toast.makeText(context, "알 수 없는 에러가 발생하였습니다.", Toast.LENGTH_LONG).show()
                     }
                 }
-            }
-
-            override fun onFailure(call: Call<AccountFindUsernameResponseDto>, t: Throwable) {
-                if(!isViewDestroyed){
+            }, { t ->
+                if(!isViewDestroyed) {
                     // 버튼 로딩 상태 해제
-                    setButtonLoading(false)
+                    setEmailInputButtonLoading(false)
 
-                    Toast.makeText(context, "요청에 실패하였습니다.", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(context, "Error: ${t.message}", Toast.LENGTH_LONG).show()
                 }
             }
-        })
+        )
     }
 
-    private fun setButtonLoading(isLoading: Boolean){
+    // 이메일 입력 버튼 프로그래스바
+    private fun setEmailInputButtonLoading(isLoading: Boolean){
         if(isLoading){
-            binding.findIdButton.apply {
+            binding.emailInputButton.apply {
                 text = ""
                 isEnabled = false
             }
-            binding.findIdProgressBar.visibility = View.VISIBLE
+            binding.emailInputProgressBar.visibility = View.VISIBLE
         }else{
-            binding.findIdButton.apply {
-                text = context?.getText(R.string.find_username)
+            binding.emailInputButton.apply {
+                text = context?.getText(R.string.find_password_email_input_button)
                 isEnabled = true
             }
-            binding.findIdProgressBar.visibility = View.GONE
+            binding.emailInputProgressBar.visibility = View.GONE
         }
     }
 
-    // 아이디 찾기 결과
-    private fun setViewForResult(username: String?){
-        binding.resultUsername.text = username
-        binding.findIdLayout.visibility = View.GONE
-        binding.resultLayout.visibility = View.VISIBLE
+
+    // 코드를 통한 비밀번호 임시 변경
+    private fun findPassword(username: String, code: String){
+        val accountFindPasswordRequestDto = AccountFindPasswordRequestDto(username, code)
+
+        // 버튼 로딩 상태
+        setCodeInputButtonLoading(true)
+
+        ServerUtil.findPassword(accountFindPasswordRequestDto,
+            { response ->
+                if(!isViewDestroyed) {
+                    // 버튼 로딩 상태 해제
+                    setCodeInputButtonLoading(false)
+
+                    if (response.isSuccessful) {
+                        setViewForResult()
+                    } else {
+                        binding.codeMessage.visibility = View.VISIBLE
+                    }
+                }
+            }, { t ->
+                if(!isViewDestroyed) {
+                    // 버튼 로딩 상태 해제
+                    setCodeInputButtonLoading(false)
+
+                    Toast.makeText(context, "Error: ${t.message}", Toast.LENGTH_LONG).show()
+                }
+            }
+        )
+    }
+
+    // 코드 입력 버튼 프로그래스바
+    private fun setCodeInputButtonLoading(isLoading: Boolean){
+        if(isLoading){
+            binding.codeInputButton.apply {
+                text = ""
+                isEnabled = false
+            }
+            binding.codeInputProgressBar.visibility = View.VISIBLE
+        }else{
+            binding.codeInputButton.apply {
+                text = context?.getText(R.string.confirm)
+                isEnabled = true
+            }
+            binding.codeInputProgressBar.visibility = View.GONE
+        }
     }
 }
