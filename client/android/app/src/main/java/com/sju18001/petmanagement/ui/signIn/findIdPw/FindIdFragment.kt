25b5ccb6b99716c455b3c0e87eb9d6efe647c611
Hanbit-Kang <58168528+Hanbit-Kang@users.noreply.github.com>
2021-07-19 21:18:11 +0900

    Implement find id fragment

--- client/android/app/src/main/java/com/sju18001/petmanagement/ui/signIn/findIdPw/FindIdFragment.kt
+++ client/android/app/src/main/java/com/sju18001/petmanagement/ui/signIn/findIdPw/FindIdFragment.kt
@@ -1,16 +1,24 @@
 package com.sju18001.petmanagement.ui.signIn.findIdPw
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Patterns
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.sju18001.petmanagement.R
+import com.sju18001.petmanagement.controller.Util
 import com.sju18001.petmanagement.databinding.FragmentFindIdBinding
 
 class FindIdFragment : Fragment(){
     private var _binding: FragmentFindIdBinding? = null
     private val binding get() = _binding!!
+
+    private val INPUT_LENGTH = 1
+    private val EMAIL = 0
+    private val isValidInput: HashMap<Int, Boolean> = HashMap()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,5 +27,50 @@ class FindIdFragment : Fragment(){
     ): View? {
         _binding = FragmentFindIdBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        // 아이디 찾기 버튼 클릭
+        binding.findIdButton.setOnClickListener{
+            activity?.let { Util().hideKeyboard(it) }
+        }
+
+        // 레이아웃 클릭
+        binding.findIdLayout.setOnClickListener{
+            activity?.let { Util().hideKeyboard(it) }
+        }
+
+        // 이메일 입력란 입력
+        binding.emailEditText.addTextChangedListener(object: TextWatcher {
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if(Patterns.EMAIL_ADDRESS.matcher(s).matches()) {
+                    isValidInput[EMAIL] = true
+                    binding.emailMessage.visibility = View.GONE
+                }
+                else {
+                    isValidInput[EMAIL] = false
+                    binding.emailMessage.visibility = View.VISIBLE
+                }
+                checkIsValid()
+            }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun afterTextChanged(s: Editable?) {}
+        })
+    }
+
+    // check for all valid(for enabling sign up button)
+    private fun checkIsValid() {
+        for(i in 0 until INPUT_LENGTH) {
+            if(!isValidInput[i]!!) {
+                // if not valid -> disable button + return
+                binding.findIdButton.isEnabled = false
+                return
+            }
+        }
+
+        // if all is valid -> enable button
+        binding.findIdButton.isEnabled = true
     }
 }
