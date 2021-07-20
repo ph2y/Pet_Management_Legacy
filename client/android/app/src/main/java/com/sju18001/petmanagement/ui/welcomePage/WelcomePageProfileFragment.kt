4659f8e2db167f79fddfd81dbbb53dcebf5fc6ab
Hanbit-Kang <58168528+Hanbit-Kang@users.noreply.github.com>
2021-07-20 21:34:53 +0900

    Implement welcome page profile fragment

--- client/android/app/src/main/java/com/sju18001/petmanagement/ui/welcomePage/WelcomePageProfileFragment.kt
+++ client/android/app/src/main/java/com/sju18001/petmanagement/ui/welcomePage/WelcomePageProfileFragment.kt
@@ -1,10 +1,12 @@
 package com.sju18001.petmanagement.ui.welcomePage
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.sju18001.petmanagement.MainActivity
 import com.sju18001.petmanagement.R
 import com.sju18001.petmanagement.databinding.FragmentWelcomePageProfileBinding
 
@@ -18,5 +20,21 @@ class WelcomePageProfileFragment : Fragment() {
     ): View? {
         _binding = FragmentWelcomePageProfileBinding.inflate(layoutInflater)
         return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        // 수락 버튼
+        binding.acceptButton.setOnClickListener{
+            // TODO: 프로필 편집 페이지로 이동
+        }
+        
+        // 거절 버튼
+        binding.declineButton.setOnClickListener{
+            val intent = Intent(context, MainActivity::class.java)
+            startActivity(intent)
+            activity?.finish()
+        }
     }
 }
