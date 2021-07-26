6475ebf290400a2bfa8105285e856199983e20b0
Hanbit-Kang <58168528+Hanbit-Kang@users.noreply.github.com>
2021-07-26 15:33:27 +0900

    Save MyPet page index

--- client/android/app/src/main/java/com/sju18001/petmanagement/ui/myPet/MyPetFragment.kt
+++ client/android/app/src/main/java/com/sju18001/petmanagement/ui/myPet/MyPetFragment.kt
@@ -1,11 +1,16 @@
 package com.sju18001.petmanagement.ui.myPet
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayoutMediator
 import com.sju18001.petmanagement.databinding.FragmentMyPetBinding
 import com.sju18001.petmanagement.ui.myPet.petManager.PetManagerFragment
@@ -29,8 +34,16 @@ class MyPetFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val tabLayout = binding.tabLayout
-        val viewPager = binding.viewPager
-        viewPager.adapter = MyPetCollectionAdapter(this)
+        val viewPager = binding.viewPager.also{
+            it.adapter = MyPetCollectionAdapter(this)
+            it.currentItem = requireActivity().intent.getIntExtra("pageIndex", 0)
+            it.registerOnPageChangeCallback(object: ViewPager2.OnPageChangeCallback() {
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+                    requireActivity().intent.putExtra("pageIndex", position)
+                }
+            })
+        }
 
         TabLayoutMediator(tabLayout, viewPager){ tab, position ->
             tab.text = TAB_ELEMENTS[position]
