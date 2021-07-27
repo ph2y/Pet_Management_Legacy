456280913e69db118bd3928623841e9d30539d07
Hanbit-Kang <58168528+Hanbit-Kang@users.noreply.github.com>
2021-07-27 21:50:20 +0900

    Add cancel, confirm button, Redesign edit pet feed schedule fragment

--- client/android/app/src/main/java/com/sju18001/petmanagement/ui/myPet/petFeedScheduler/EditPetFeedScheduleFragment.kt
+++ client/android/app/src/main/java/com/sju18001/petmanagement/ui/myPet/petFeedScheduler/EditPetFeedScheduleFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.sju18001.petmanagement.R
 import com.sju18001.petmanagement.databinding.FragmentAddEditPetBinding
 import com.sju18001.petmanagement.databinding.FragmentEditPetFeedScheduleBinding
@@ -13,16 +14,26 @@ class EditPetFeedScheduleFragment : Fragment() {
     private var _binding: FragmentEditPetFeedScheduleBinding? = null
     private val binding get() = _binding!!
 
+    // 리싸이클러뷰
+    private lateinit var adapter: PetNameListAdapter
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         _binding = FragmentEditPetFeedScheduleBinding.inflate(inflater, container, false)
 
+        // 뒤로가기 버튼
         binding.backButton.setOnClickListener {
             activity?.finish()
         }
+
+        // 리싸이클러뷰
+        val dataSet = arrayListOf(PetNameListItem("냠냠미"), PetNameListItem("몀몀미"))
+
+        adapter = PetNameListAdapter(dataSet)
+        binding.petNameListRecyclerView.adapter = adapter
+        binding.petNameListRecyclerView.layoutManager = LinearLayoutManager(activity)
 
         return binding.root
     }
