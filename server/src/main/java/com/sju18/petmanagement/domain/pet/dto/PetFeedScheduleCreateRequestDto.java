ca0c1022d52b32dfac15cb66564a80c704e1a164
Komputer-P <wlghks0314@gmail.com>
2021-07-29 15:39:39 +0900

    PetFeedSchedule & Pet Entity update/hotfix + related dto change

--- server/src/main/java/com/sju18/petmanagement/domain/pet/dto/PetFeedScheduleCreateRequestDto.java
+++ server/src/main/java/com/sju18/petmanagement/domain/pet/dto/PetFeedScheduleCreateRequestDto.java
@@ -1,10 +1,11 @@
 package com.sju18.petmanagement.domain.pet.dto;
 
 import lombok.Data;
+import java.util.Set;
 
 @Data
 public class PetFeedScheduleCreateRequestDto {
-    private Long pet_id;
+    private Set<Long> pet_id;
     private String feed_time;
     private String memo;
 }
--- server/src/main/java/com/sju18/petmanagement/domain/pet/dto/PetFeedScheduleUpdateRequestDto.java
+++ server/src/main/java/com/sju18/petmanagement/domain/pet/dto/PetFeedScheduleUpdateRequestDto.java
@@ -1,11 +1,12 @@
 package com.sju18.petmanagement.domain.pet.dto;
 
 import lombok.Data;
+import java.util.Set;
 
 @Data
 public class PetFeedScheduleUpdateRequestDto {
     private Long id;
-    private Long pet_id;
+    private Set<Long> pet_id;
     private String feed_time;
     private String memo;
     private Boolean is_turned_on;
