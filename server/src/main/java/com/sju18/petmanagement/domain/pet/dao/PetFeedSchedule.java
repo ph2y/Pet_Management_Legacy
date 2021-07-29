33b323ec937bb9a13045cd1afd550a1008a94c0b
Komputer-P <wlghks0314@gmail.com>
2021-07-29 16:39:06 +0900

    PetFeedSchedule pet_id_list type changed

--- server/src/main/java/com/sju18/petmanagement/domain/pet/dao/PetFeedSchedule.java
+++ server/src/main/java/com/sju18/petmanagement/domain/pet/dao/PetFeedSchedule.java
@@ -6,8 +6,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import javax.persistence.*;
 import java.time.LocalTime;
-import java.util.HashSet;
-import java.util.Set;
 
 @Getter
 @Setter
@@ -23,8 +21,7 @@ public class PetFeedSchedule {
     private String username;
 
     @Column(nullable = false)
-    @ElementCollection
-    private Set<Long> pet_id = new HashSet<>();
+    private String pet_id_list;
 
     @Column
     private LocalTime feed_time;
@@ -32,9 +29,9 @@ public class PetFeedSchedule {
     private Boolean is_turned_on;
 
     @Builder
-    public PetFeedSchedule(String username, Set<Long> pet_id, LocalTime feed_time, String memo, Boolean is_turned_on) {
+    public PetFeedSchedule(String username, String pet_id_list, LocalTime feed_time, String memo, Boolean is_turned_on) {
         this.username = username;
-        this.pet_id = pet_id;
+        this.pet_id_list = pet_id_list;
         this.feed_time = feed_time;
         this.memo = memo;
         this.is_turned_on = is_turned_on;
--- server/src/main/java/com/sju18/petmanagement/domain/pet/dto/PetFeedScheduleUpdateRequestDto.java
+++ server/src/main/java/com/sju18/petmanagement/domain/pet/dto/PetFeedScheduleUpdateRequestDto.java
@@ -1,12 +1,11 @@
 package com.sju18.petmanagement.domain.pet.dto;
 
 import lombok.Data;
-import java.util.Set;
 
 @Data
 public class PetFeedScheduleUpdateRequestDto {
     private Long id;
-    private Set<Long> pet_id;
+    private String pet_id_list;
     private String feed_time;
     private String memo;
     private Boolean is_turned_on;
