3d7bb41c44259453b067962b4175ad0cbebb18e5
Seongmin Yun <smyun99@gmail.com>
2021-07-30 22:53:52 +0900

    Implement MessageSource

---
+++ server/src/main/java/com/sju18/petmanagement/global/util/message/MessageService.java
@@ -0,0 +1,36 @@
+package com.sju18.petmanagement.global.util.message;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
+
+@Configuration
+public class MessageService implements WebMvcConfigurer {
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver localResolver=new SessionLocaleResolver();
+        localResolver.setDefaultLocale(Locale.US);
+        return localResolver;
+    }
+
+    @Bean
+    public MessageSource accountMsgSrc() {
+        ReloadableResourceBundleMessageSource msgSrc = new ReloadableResourceBundleMessageSource();
+        msgSrc.setBasename("classpath:/static/messages/account");
+        return msgSrc;
+    }
+
+    @Bean
+    public MessageSource petMsgSrc() {
+        ReloadableResourceBundleMessageSource msgSrc = new ReloadableResourceBundleMessageSource();
+        msgSrc.setBasename("classpath:/static/messages/pet");
+        return msgSrc;
+    }
+}
