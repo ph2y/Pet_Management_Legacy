a72c5350944505892bffa8fda91b4d61f2232c80
Seongmin Yun <smyun99@gmail.com>
2021-07-31 02:27:10 +0900

    Refactoring Account/CreateAccount: Add errorCode enum

--- server/src/main/java/com/sju18/petmanagement/global/exception/DbException.java
+++ server/src/main/java/com/sju18/petmanagement/global/exception/DbException.java
@@ -4,9 +4,9 @@ import com.sju18.petmanagement.global.util.error.ErrorCode;
 import lombok.Getter;
 
 @Getter
-public class DtoValidityException extends Exception {
-    private final ErrorCode errorCode = ErrorCode.DTO_VALIDATION;
-    public DtoValidityException(String msg) {
+public class DbException extends Exception {
+    private final ErrorCode errorCode = ErrorCode.DATABASE;
+    public DbException(String msg) {
         super(msg);
     }
 }
