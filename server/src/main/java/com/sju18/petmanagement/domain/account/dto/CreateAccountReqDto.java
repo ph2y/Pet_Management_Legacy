047abb05bb1c2124ba1d3ae414984d3eee193273
Seongmin Yun <smyun99@gmail.com>
2021-07-31 01:45:22 +0900

    Refactoring Account/CreateAccount: Impi req validation logic

--- server/src/main/java/com/sju18/petmanagement/domain/account/dto/CreateAccountReqDto.java
+++ server/src/main/java/com/sju18/petmanagement/domain/account/dto/CreateAccountReqDto.java
@@ -2,15 +2,35 @@ package com.sju18.petmanagement.domain.account.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import javax.validation.constraints.Email;
+
 @Data
 public class CreateAccountReqDto {
-    @NonNull
+    @NotBlank(message = "{valid.username.blank}")
+    @Size(min = 5, max = 20, message = "{valid.username.size}")
     private String username;
+
+    @NotBlank(message = "{valid.password.blank}")
+    @Size(min = 8, max = 20, message = "{valid.password.size}")
     private String password;
+
+    @Email(message = "{valid.email.email}")
+    @NotBlank(message = "{valid.email.blank}")
+    @Size(max = 50, message = "{valid.email.size}")
     private String email;
+
+    @Size(max = 20, message = "{valid.nickname.size}")
     private String nickname;
+
+    @NotBlank(message = "{valid.phone.blank}")
+    @Size(min = 10, max = 11, message = "{valid.phone.size}")
     private String phone;
-    private String photoUrl;
+
+    @NotBlank(message = "{valid.marketing.blank}")
     private Boolean marketing;
+
+    @Size(max = 200, message = "{valid.userMessage.size}")
     private String userMessage;
 }
