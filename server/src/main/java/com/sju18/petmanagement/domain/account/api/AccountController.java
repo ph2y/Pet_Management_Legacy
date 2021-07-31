29708507b9698ff6b3bee766f34e753da18e4c8e
Seongmin Yun <smyun99@gmail.com>
2021-07-31 16:22:00 +0900

    Refactoring Account/UpdateAccount: refactoring uploadProfilePhoto

--- server/src/main/java/com/sju18/petmanagement/domain/account/api/AccountController.java
+++ server/src/main/java/com/sju18/petmanagement/domain/account/api/AccountController.java
@@ -1,7 +1,6 @@
 package com.sju18.petmanagement.domain.account.api;
 
 import com.sju18.petmanagement.domain.account.application.AccountService;
-import com.sju18.petmanagement.domain.account.dao.Account;
 import com.sju18.petmanagement.domain.account.dto.*;
 import com.sju18.petmanagement.global.common.DtoMetadata;
 import com.sju18.petmanagement.global.util.message.MessageConfig;
@@ -12,12 +11,9 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.context.MessageSource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import javax.validation.Valid;
@@ -55,36 +51,23 @@ public class AccountController {
             dtoMetadata = new DtoMetadata(e.getMessage(), e.getClass().getName());
             return ResponseEntity.status(400).body(new UpdateAccountResDto(dtoMetadata));
         }
-        dtoMetadata = new DtoMetadata(msgSrc.getMessage("res.update.success", null, Locale.ENGLISH);
+        dtoMetadata = new DtoMetadata(msgSrc.getMessage("res.update.success", null, Locale.ENGLISH));
         return ResponseEntity.ok(new UpdateAccountResDto(dtoMetadata));
     }
 
     @PostMapping("/api/account/uploadprofilephoto")
-    public ResponseEntity<?> uploadProfilePhoto(Authentication authentication, MultipartHttpServletRequest fileRequest) {
-        // 유저 ID 인출
-        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-        String currentUserName = userDetails.getUsername();
-        Account currentUserProfile;
+    public ResponseEntity<?> uploadProfilePhoto(Authentication auth, MultipartHttpServletRequest fileReq) {
+        DtoMetadata dtoMetadata;
         String fileUrl;
         try {
-            currentUserProfile = accountRepository.findByUsername(currentUserName)
-                    .orElseThrow(() -> new UsernameNotFoundException(currentUserName));
-
-            // 첨부파일 인출
-            MultipartFile uploadedFile = fileRequest.getFile("file");
-
-            // 해당 유저의 계정 스토리지에 프로필 사진 저장
-            fileUrl = fileService.saveAccountProfilePhoto(currentUserProfile.getId() ,uploadedFile);
-
-            // DB 데이터 업데이트
-            currentUserProfile.setPhotoUrl(fileUrl);
-            accountRepository.save(currentUserProfile);
+            fileUrl = accountServ.updateAccountPhoto(auth, fileReq);
         } catch (Exception e) {
-            return ResponseEntity.badRequest().body(new UploadProfilePhotoResponseDto("null", e.getMessage()));
+            logger.warn(e.toString());
+            dtoMetadata = new DtoMetadata(e.getMessage(), e.getClass().getName());
+            return ResponseEntity.status(400).body(new UpdateAccountPhotoResDto(dtoMetadata, null));
         }
-        return ResponseEntity.ok(
-                new UploadProfilePhotoResponseDto(fileUrl, "Profile Photo successfully uploaded")
-        );
+        dtoMetadata = new DtoMetadata(msgSrc.getMessage("res.updatePhoto.success", null, Locale.ENGLISH));
+        return ResponseEntity.ok(new UpdateAccountPhotoResDto(dtoMetadata, fileUrl));
     }
 }
 
