e7116c0e4b4b4fb9c7ffbda7cd3b3c90a3f903eb
Seongmin Yun <smyun99@gmail.com>
2021-07-31 15:20:25 +0900

    Refactoring Account/UpdateAccount: refactoring accountProfileUpdate

--- server/src/main/java/com/sju18/petmanagement/domain/account/application/AccountService.java
+++ server/src/main/java/com/sju18/petmanagement/domain/account/application/AccountService.java
@@ -3,12 +3,16 @@ package com.sju18.petmanagement.domain.account.application;
 import com.sju18.petmanagement.domain.account.dao.Account;
 import com.sju18.petmanagement.domain.account.dao.AccountRepository;
 import com.sju18.petmanagement.domain.account.dto.CreateAccountReqDto;
+import com.sju18.petmanagement.domain.account.dto.UpdateAccountReqDto;
 import com.sju18.petmanagement.global.exception.DtoValidityException;
 import com.sju18.petmanagement.global.util.message.MessageConfig;
 import com.sju18.petmanagement.global.util.storage.FileService;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.MessageSource;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -26,7 +30,7 @@ public class AccountService {
     @Transactional
     public void createAccount(CreateAccountReqDto reqDto) throws Exception {
         // 중복 확인
-        this.checkDuplication(reqDto);
+        this.checkDuplication(reqDto.getUsername(), reqDto.getEmail(), reqDto.getPhone());
 
         // Account 객체 생성
         Account newAccount = Account.builder()
@@ -46,23 +50,62 @@ public class AccountService {
         fileService.createAccountFileStorage(newAccount.getId());
     }
 
-    private void checkDuplication(CreateAccountReqDto reqDto) throws DtoValidityException {
+    private void checkDuplication(String username, String email, String phone) throws DtoValidityException {
         // 중복 확인
-        if (accountRepository.existsByEmail(reqDto.getEmail())) {
-            throw new DtoValidityException(
-                    msgSrc.getMessage("error.dup.email",null, Locale.ENGLISH)
-            );
-        }
-        if (accountRepository.existsByUsername(reqDto.getUsername())) {
+        if (username != null && accountRepository.existsByUsername(username)) {
             throw new DtoValidityException(
                     msgSrc.getMessage("error.dup.username",null,Locale.ENGLISH)
             );
         }
-        if (accountRepository.existsByPhone(reqDto.getPhone())) {
+        if (email != null && accountRepository.existsByEmail(email)) {
+            throw new DtoValidityException(
+                    msgSrc.getMessage("error.dup.email",null, Locale.ENGLISH)
+            );
+        }
+        if (phone != null && accountRepository.existsByPhone(phone)) {
             throw new DtoValidityException(
                     msgSrc.getMessage("error.dup.phone",null,Locale.ENGLISH)
             );
         }
+    }
+
+    @Transactional
+    public void updateAccount(Authentication auth, UpdateAccountReqDto reqDto) throws Exception {
+        // 로그인된 현재 사용자 정보 조회
+        UserDetails userDetails = (UserDetails) auth.getPrincipal();
+        String currentUsername = userDetails.getUsername();
+        Account currentAccount;
+
+        // 중복 확인
+        this.checkDuplication(null, reqDto.getEmail(), reqDto.getPhone());
+
+        // 기존 사용자 프로필 로드
+        currentAccount = accountRepository.findByUsername(currentUsername)
+                .orElseThrow(() -> new UsernameNotFoundException(currentUsername));
+
+        // 기존 사용자 프로필 중 변경사항이 있는 필드 업데이트
+        if (reqDto.getEmail() != null) {
+            currentAccount.setEmail(reqDto.getEmail());
+        }
+        if (reqDto.getPhone() != null) {
+            currentAccount.setPhone(reqDto.getPhone());
+        }
+        if (reqDto.getMarketing() != null) {
+            currentAccount.setMarketing(reqDto.getMarketing());
+        }
+        if (reqDto.getNickname() != null) {
+            currentAccount.setNickname(reqDto.getNickname());
+        }
+        if (reqDto.getUserMessage() != null) {
+            currentAccount.setUserMessage(reqDto.getUserMessage());
+        }
+
+        // 기존 사용자 정보 변경사항 적용
+        accountRepository.save(currentAccount);
+    }
+
+    public Long getCurrentAccountId(Authentication auth) {
+        UserDetails userDetails = (UserDetails) auth.getPrincipal();
     }
 
 }
--- server/src/main/java/com/sju18/petmanagement/domain/account/dto/UpdateAccountReqDto.java
+++ server/src/main/java/com/sju18/petmanagement/domain/account/dto/UpdateAccountReqDto.java
@@ -2,36 +2,21 @@ package com.sju18.petmanagement.domain.account.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import javax.validation.constraints.Email;
 
 @Data
-public class CreateAccountReqDto {
-    @NotBlank(message = "valid.username.blank")
-    @Size(min = 5, max = 20, message = "valid.username.size")
-    private String username;
-
-    @NotBlank(message = "valid.password.blank")
-    @Size(min = 8, max = 20, message = "valid.password.size")
-    private String password;
-
+public class UpdateAccountReqDto {
     @Email(message = "valid.email.email")
-    @NotBlank(message = "valid.email.blank")
     @Size(max = 50, message = "valid.email.size")
     private String email;
-
-    @NotBlank(message = "valid.phone.blank")
     @Size(min = 12, max = 13, message = "valid.phone.size")
     private String phone;
-
     @Size(max = 20, message = "valid.nickname.size")
     private String nickname;
-
-    @NotNull(message = "valid.marketing.null")
     private Boolean marketing;
-
     @Size(max = 200, message = "valid.userMessage.size")
     private String userMessage;
 }
