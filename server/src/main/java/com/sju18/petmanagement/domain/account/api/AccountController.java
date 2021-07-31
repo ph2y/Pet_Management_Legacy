964c4f08b6468e198cb0a46ce6b6a0c2963f9763
Seongmin Yun <smyun99@gmail.com>
2021-07-31 15:20:25 +0900

    Refactoring Account/UpdateAccount: refactoring accountProfileUpdate

--- server/src/main/java/com/sju18/petmanagement/domain/account/api/AccountController.java
+++ server/src/main/java/com/sju18/petmanagement/domain/account/api/AccountController.java
@@ -1,8 +1,8 @@
 package com.sju18.petmanagement.domain.account.api;
 
 import com.sju18.petmanagement.domain.account.application.AccountService;
-import com.sju18.petmanagement.domain.account.dto.CreateAccountReqDto;
-import com.sju18.petmanagement.domain.account.dto.CreateAccountResDto;
+import com.sju18.petmanagement.domain.account.dao.Account;
+import com.sju18.petmanagement.domain.account.dto.*;
 import com.sju18.petmanagement.global.common.DtoMetadata;
 import com.sju18.petmanagement.global.util.message.MessageConfig;
 
@@ -11,9 +11,14 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.context.MessageSource;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import javax.validation.Valid;
 import java.util.Locale;
@@ -27,12 +32,11 @@ public class AccountController {
 
 
     @PostMapping("/api/account/create")
-    public ResponseEntity<?> registerAccount(@Valid @RequestBody CreateAccountReqDto reqDto) {
+    public ResponseEntity<?> createAccount(@Valid @RequestBody CreateAccountReqDto reqDto) {
         DtoMetadata dtoMetadata;
         try {
             accountServ.createAccount(reqDto);
         } catch (Exception e) {
-            // 예외 처리
             logger.warn(e.toString());
             dtoMetadata = new DtoMetadata(e.getMessage(), e.getClass().getName());
             return ResponseEntity.status(400).body(new CreateAccountResDto(dtoMetadata));
@@ -41,6 +45,47 @@ public class AccountController {
         return ResponseEntity.ok(new CreateAccountResDto(dtoMetadata));
     }
 
+    @PostMapping("/api/account/update")
+    public ResponseEntity<?> updateAccount(Authentication auth, @Valid @RequestBody UpdateAccountReqDto reqDto) {
+        DtoMetadata dtoMetadata;
+        try {
+            accountServ.updateAccount(auth, reqDto);
+        } catch (Exception e) {
+            logger.warn(e.toString());
+            dtoMetadata = new DtoMetadata(e.getMessage(), e.getClass().getName());
+            return ResponseEntity.status(400).body(new UpdateAccountResDto(dtoMetadata));
+        }
+        dtoMetadata = new DtoMetadata(msgSrc.getMessage("res.update.success", null, Locale.ENGLISH);
+        return ResponseEntity.ok(new UpdateAccountResDto(dtoMetadata));
+    }
+
+    @PostMapping("/api/account/uploadprofilephoto")
+    public ResponseEntity<?> uploadProfilePhoto(Authentication authentication, MultipartHttpServletRequest fileRequest) {
+        // 유저 ID 인출
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        String currentUserName = userDetails.getUsername();
+        Account currentUserProfile;
+        String fileUrl;
+        try {
+            currentUserProfile = accountRepository.findByUsername(currentUserName)
+                    .orElseThrow(() -> new UsernameNotFoundException(currentUserName));
+
+            // 첨부파일 인출
+            MultipartFile uploadedFile = fileRequest.getFile("file");
+
+            // 해당 유저의 계정 스토리지에 프로필 사진 저장
+            fileUrl = fileService.saveAccountProfilePhoto(currentUserProfile.getId() ,uploadedFile);
+
+            // DB 데이터 업데이트
+            currentUserProfile.setPhotoUrl(fileUrl);
+            accountRepository.save(currentUserProfile);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(new UploadProfilePhotoResponseDto("null", e.getMessage()));
+        }
+        return ResponseEntity.ok(
+                new UploadProfilePhotoResponseDto(fileUrl, "Profile Photo successfully uploaded")
+        );
+    }
 }
 
 
--- server/src/main/java/com/sju18/petmanagement/domain/account/api/ProfileUpdateController.java
+++ server/src/main/java/com/sju18/petmanagement/domain/account/api/ProfileUpdateController.java
@@ -2,8 +2,8 @@ package com.sju18.petmanagement.domain.account.api;
 
 import com.sju18.petmanagement.domain.account.dao.Account;
 import com.sju18.petmanagement.domain.account.dao.AccountRepository;
-import com.sju18.petmanagement.domain.account.dto.ProfileUpdateResponseDto;
-import com.sju18.petmanagement.domain.account.dto.ProfileUpdateRequestDto;
+import com.sju18.petmanagement.domain.account.dto.UpdateAccountResDto;
+import com.sju18.petmanagement.domain.account.dto.UpdateAccountReqDto;
 import com.sju18.petmanagement.domain.account.dto.UploadProfilePhotoResponseDto;
 import com.sju18.petmanagement.global.util.storage.FileService;
 import lombok.RequiredArgsConstructor;
@@ -53,46 +53,5 @@ public class ProfileUpdateController {
         return ResponseEntity.ok(
                 new UploadProfilePhotoResponseDto(fileUrl, "Profile Photo successfully uploaded")
         );
-    }
-
-    @PostMapping("/api/account/profileupdate")
-    public ResponseEntity<?> updateAccountProfile(Authentication authentication, @RequestBody ProfileUpdateRequestDto profileUpdateRequestDto) {
-        // 로그인된 현재 사용자 정보 조회
-        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-        String currentUserName = userDetails.getUsername();
-        Account currentUserProfile;
-        try {
-            // 기존 사용자 프로필
-            currentUserProfile = accountRepository.findByUsername(currentUserName)
-                    .orElseThrow(() -> new UsernameNotFoundException(currentUserName));
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(new ProfileUpdateResponseDto(e.getMessage()));
-        }
-
-        // 기존 사용자 프로필 중 변경사항이 있는 필드 업데이트
-        if (profileUpdateRequestDto.getEmail() != null) {
-            currentUserProfile.setEmail(profileUpdateRequestDto.getEmail());
-        }
-        if (profileUpdateRequestDto.getNickname() != null) {
-            currentUserProfile.setNickname(profileUpdateRequestDto.getNickname());
-        }
-        if (profileUpdateRequestDto.getPhone() != null) {
-            currentUserProfile.setPhone(profileUpdateRequestDto.getPhone());
-        }
-        if (profileUpdateRequestDto.getUserMessage() != null) {
-            currentUserProfile.setUserMessage(profileUpdateRequestDto.getUserMessage());
-        }
-        // 프로필 사진 변경 기능은 파일을 다루어야 함으로 장래 변경해야됨.
-        if (profileUpdateRequestDto.getPhoto() != null) {
-            currentUserProfile.setPhotoUrl(profileUpdateRequestDto.getPhoto());
-        }
-
-        // 기존 사용자 정보 변경사항 적용
-        try {
-            accountRepository.save(currentUserProfile);
-            return ResponseEntity.ok(new ProfileUpdateResponseDto("Account profile update success"));
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(new ProfileUpdateResponseDto(e.getMessage()));
-        }
     }
 }
