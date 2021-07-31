b4140bd2f75cc2a8625cdb3b09fc6193a7eb09d4
Seongmin Yun <smyun99@gmail.com>
2021-08-01 05:32:10 +0900

    Refactoring FileService: improve fileService Exception message & finalize server refactoring

--- server/src/main/java/com/sju18/petmanagement/global/storage/IllegalFileExtensionException.java
+++
@@ -1,7 +0,0 @@
-package com.sju18.petmanagement.global.storage;
-
-public class IllegalFileExtensionException extends Exception {
-    public IllegalFileExtensionException(String filename) {
-        super("This file violates file extension limit policy: " + filename);
-    }
-}
