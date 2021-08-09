b7a03bf43f2e357b11693782242d80cabd93a07a
Hanbit-Kang <58168528+Hanbit-Kang@users.noreply.github.com>
2021-08-09 23:25:11 +0900

    Implement community comment item

--- client/android/app/src/main/java/com/sju18001/petmanagement/ui/community/comment/CommunityCommentFragment.kt
+++ client/android/app/src/main/java/com/sju18001/petmanagement/ui/community/comment/CommunityCommentFragment.kt
@@ -7,6 +7,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.sju18001.petmanagement.databinding.FragmentCommunityCommentBinding
+import com.sju18001.petmanagement.restapi.dao.Comment
+import com.sju18001.petmanagement.restapi.dao.Post
 
 class CommunityCommentFragment : Fragment() {
 
@@ -39,5 +41,22 @@ class CommunityCommentFragment : Fragment() {
             it.adapter = adapter
             it.layoutManager = LinearLayoutManager(activity)
         }
+
+        updateComments()
+    }
+
+    private fun updateComments(){
+        val items = getFetchedComment()
+        adapter.addItems(items)
+        binding.commentRecyclerView.post{
+            adapter.notifyDataSetChanged()
+        }
+    }
+
+    private fun getFetchedComment(): List<Comment>{
+        // TODO: 서버의 fetch comment 기능이 구현되면 적용할 것
+        val item1 = Comment("default", "liszt", "불닭볶음면 맛있네요..^^", "6시간")
+        val item2 = Comment("default", "franz", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.", "5시간")
+        return listOf(item1, item2, item1, item2, item1, item1, item2, item1, item2, item1, item1, item2, item1, item2, item1)
     }
 }
