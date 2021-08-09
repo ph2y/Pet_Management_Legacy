265211888a699e478b5300dd36df772eccd204a5
Hanbit-Kang <58168528+Hanbit-Kang@users.noreply.github.com>
2021-08-09 23:49:49 +0900

    Add listener on views in community comment fragment

--- client/android/app/src/main/java/com/sju18001/petmanagement/ui/community/comment/CommunityCommentFragment.kt
+++ client/android/app/src/main/java/com/sju18001/petmanagement/ui/community/comment/CommunityCommentFragment.kt
@@ -1,11 +1,13 @@
 package com.sju18001.petmanagement.ui.community.comment
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.sju18001.petmanagement.controller.Util
 import com.sju18001.petmanagement.databinding.FragmentCommunityCommentBinding
 import com.sju18001.petmanagement.restapi.dao.Comment
 import com.sju18001.petmanagement.restapi.dao.Post
@@ -27,6 +29,9 @@ class CommunityCommentFragment : Fragment() {
         // 어뎁터 초기화
         initializeAdapter()
 
+        // 리스너 추가
+        setListenerOnViews()
+
         return binding.root
     }
 
@@ -37,11 +42,18 @@ class CommunityCommentFragment : Fragment() {
 
     private fun initializeAdapter(){
         adapter = CommunityCommentListAdapter(arrayListOf())
+        adapter.communityCommentListAdapterInterface = object: CommunityCommentListAdapterInterface{
+            override fun getActivity(): Activity {
+                return requireActivity()
+            }
+        }
+
         binding.recyclerViewComment?.let{
             it.adapter = adapter
             it.layoutManager = LinearLayoutManager(activity)
         }
 
+        // 초기 댓글 추가
         updateComments()
     }
 
@@ -57,6 +69,18 @@ class CommunityCommentFragment : Fragment() {
         // TODO: 서버의 fetch comment 기능이 구현되면 적용할 것
         val item1 = Comment("default", "liszt", "불닭볶음면 맛있네요..^^", "6시간")
         val item2 = Comment("default", "franz", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.", "5시간")
-        return listOf(item1, item2, item1, item2, item1, item1, item2, item1, item2, item1, item1, item2, item1, item2, item1)
+        return listOf(item1, item2, item1, item2, item1, item1, item2, item1, item2, item1)
+    }
+
+    private fun setListenerOnViews(){
+        // 뒤로가기 버튼
+        binding.buttonBack.setOnClickListener {
+            activity?.finish()
+        }
+
+        // 편의 기능: 키보드 내리기
+        binding.layoutBackButton.setOnClickListener {
+            Util.hideKeyboard(requireActivity())
+        }
     }
 }
