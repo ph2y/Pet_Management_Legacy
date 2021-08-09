b7f5ea8946d08db2d092177837a0f3fc4ff2c931
HanJuK <qwerty03081@gmail.com>
2021-08-09 23:02:23 +0900

    layout for image/video select dialog

--- client/android/app/src/main/java/com/sju18001/petmanagement/ui/community/createUpdatePost/CreateUpdatePostFragment.kt
+++ client/android/app/src/main/java/com/sju18001/petmanagement/ui/community/createUpdatePost/CreateUpdatePostFragment.kt
@@ -1,21 +1,53 @@
 package com.sju18001.petmanagement.ui.community.createUpdatePost
 
+import android.app.Dialog
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import com.sju18001.petmanagement.R
 import com.sju18001.petmanagement.databinding.FragmentCreateUpdatePostBinding
+import com.sju18001.petmanagement.restapi.SessionManager
 
 class CreateUpdatePostFragment : Fragment() {
+
+    // constant variables
+    private val PICK_PHOTO = 0
+    private val PICK_VIDEO = 1
+
+    // variables for view binding
     private var _binding: FragmentCreateUpdatePostBinding? = null
     private val binding get() = _binding!!
+
+    // variable for ViewModel
+    val createUpdatePostViewModel: CreateUpdatePostViewModel by activityViewModels()
+
+    // variables for storing API call(for cancel)
+    // TODO
+
+    // session manager for user token
+    private lateinit var sessionManager: SessionManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // get session manager
+        sessionManager = context?.let { SessionManager(it) }!!
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        // view binding
         _binding = FragmentCreateUpdatePostBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -23,9 +55,54 @@ class CreateUpdatePostFragment : Fragment() {
     override fun onStart() {
         super.onStart()
 
+        // for view restore
+        // TODO
+
+        // for post image/video picker
+        binding.uploadPhotosAndVideosButton.setOnClickListener {
+            val dialog = Dialog(requireActivity())
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+            dialog.setContentView(R.layout.select_photo_video_dialog)
+            dialog.show()
+
+            dialog.findViewById<Button>(R.id.upload_photo_button).setOnClickListener {
+                dialog.dismiss()
+
+                val intent = Intent()
+                intent.type = "image/*"
+                intent.action = Intent.ACTION_GET_CONTENT
+                startActivityForResult(Intent.createChooser(intent, "사진 선택"), PICK_PHOTO)
+            }
+            dialog.findViewById<Button>(R.id.upload_video_button).setOnClickListener {
+                dialog.dismiss()
+
+                val intent = Intent()
+                intent.type = "video/*"
+                intent.action = Intent.ACTION_GET_CONTENT
+                startActivityForResult(Intent.createChooser(intent, "동영상 선택"), PICK_VIDEO)
+            }
+        }
+
         // for back button
         binding.backButton.setOnClickListener {
             activity?.finish()
+        }
+    }
+
+    // for image/video select
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // get + save pet photo value
+        if(resultCode == AppCompatActivity.RESULT_OK && requestCode == PICK_PHOTO) {
+            if(data != null) {
+                Log.d("test", "image")
+            }
+        }
+        else if(resultCode == AppCompatActivity.RESULT_OK && requestCode == PICK_VIDEO) {
+            if(data != null) {
+                Log.d("test", "video")
+            }
         }
     }
 
