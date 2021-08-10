1aa6bc17b30cc21fa8c36af7a02c4e6f9430a81d
Seongmin Yun <smyun99@gmail.com>
2021-08-11 02:03:00 +0900

    Implement community post CRUD logic

--- server/src/main/java/com/sju18/petmanagement/domain/community/dao/PostRepository.java
+++ server/src/main/java/com/sju18/petmanagement/domain/community/dao/PostRepository.java
@@ -1,4 +1,25 @@
 package com.sju18.petmanagement.domain.community.dao;
 
-public interface PostRepository {
+import com.sju18.petmanagement.domain.account.dao.Account;
+import com.sju18.petmanagement.domain.pet.dao.Pet;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+import java.util.Optional;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+    Page<Post> findAllByPet(Pet taggedPet, Pageable pageable);
+    Optional<Post> findById(Long id);
+    Optional<Post> findByAuthorAndId(Account author, Long id);
+    @Query(
+            value = "SELECT p FROM Post AS p WHERE p.disclosure=\"PUBLIC\" OR (p.disclosure=\"FRIEND\" AND p.author IN :friends)",
+            countQuery = "SELECT COUNT(p) FROM Post AS p WHERE p.disclosure=\"PUBLIC\" OR (p.disclosure=\"FRIEND\" AND p.author IN :friends)"
+    )
+    Page<Post> findMainPosts(@Param("friends") Collection<Account> friends, Pageable pageable);
 }
--- server/src/main/java/com/sju18/petmanagement/domain/community/dto/FetchPostReqDto.java
+++ server/src/main/java/com/sju18/petmanagement/domain/community/dto/FetchPostReqDto.java
@@ -1,4 +1,15 @@
 package com.sju18.petmanagement.domain.community.dto;
 
+import lombok.Data;
+
+import javax.validation.constraints.PositiveOrZero;
+
+@Data
 public class FetchPostReqDto {
+    @PositiveOrZero(message = "valid.post.pageIndex.notNegative")
+    private Integer pageIndex;
+    @PositiveOrZero(message = "valid.post.petId.notNegative")
+    private Long petId;
+    @PositiveOrZero(message = "valid.post.id.notNegative")
+    private Long id;
 }
