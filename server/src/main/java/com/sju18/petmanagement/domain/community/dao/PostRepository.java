fd2a4562e0227b8c09f12194490ed30e90b2fb44
Seongmin Yun <smyun99@gmail.com>
2021-08-11 04:19:52 +0900

    Fix pet feed fetch logic & fix cascade issue

--- server/src/main/java/com/sju18/petmanagement/domain/community/dao/PostRepository.java
+++ server/src/main/java/com/sju18/petmanagement/domain/community/dao/PostRepository.java
@@ -1,7 +1,6 @@
 package com.sju18.petmanagement.domain.community.dao;
 
 import com.sju18.petmanagement.domain.account.dao.Account;
-import com.sju18.petmanagement.domain.pet.dao.Pet;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -14,12 +13,22 @@ import java.util.Optional;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
-    Page<Post> findAllByPet(Pet taggedPet, Pageable pageable);
     Optional<Post> findById(Long id);
     Optional<Post> findByAuthorAndId(Account author, Long id);
     @Query(
-            value = "SELECT p FROM Post AS p WHERE p.disclosure=\"PUBLIC\" OR (p.disclosure=\"FRIEND\" AND p.author IN :friends)",
-            countQuery = "SELECT COUNT(p) FROM Post AS p WHERE p.disclosure=\"PUBLIC\" OR (p.disclosure=\"FRIEND\" AND p.author IN :friends)"
+            value = "SELECT * FROM Post AS p WHERE p.pet_id=:petId",
+            countQuery = "SELECT COUNT(*) FROM Post AS p WHERE p.pet_id=:petId",
+            nativeQuery = true
     )
-    Page<Post> findMainPosts(@Param("friends") Collection<Account> friends, Pageable pageable);
+    Page<Post> findAllByTaggedPetId(@Param("petId") Long taggedPetId, Pageable pageable);
+    @Query(
+            value = "SELECT * FROM Post AS p WHERE p.disclosure=\"PUBLIC\" OR (p.disclosure=\"FRIEND\" AND p.account_id IN :friends) OR (p.disclosure=\"PRIVATE\" AND p.account_id=:me)",
+            countQuery = "SELECT COUNT(*) FROM Post AS p WHERE p.disclosure=\"PUBLIC\" OR (p.disclosure=\"FRIEND\" AND p.account_id IN :friends) OR (p.disclosure=\"PRIVATE\" AND p.account_id=:me)",
+            nativeQuery = true
+    )
+    Page<Post> findMainPosts(
+            @Param("friends") Collection<Long> friendAccountIdList,
+            @Param("me") Long myAccountId,
+            Pageable pageable
+    );
 }
