fcc3d5ea14717ee1cd44caa75136366acd1af13e
Seongmin Yun <smyun99@gmail.com>
2021-08-13 12:19:07 +0900

    Implement Like CRD Logic

--- server/src/main/java/com/sju18/petmanagement/domain/community/dao/LikeRepository.java
+++ server/src/main/java/com/sju18/petmanagement/domain/community/dao/LikeRepository.java
@@ -1,4 +1,17 @@
 package com.sju18.petmanagement.domain.community.dao;
 
-public interface LikeRepository {
+import com.sju18.petmanagement.domain.account.dao.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface LikeRepository extends JpaRepository<Like, Long> {
+    Long countAllByLikedPostId(Long likedPostId);
+    Long countAllByLikedCommentId(Long likedCommentId);
+    Optional<Like> findByLikedPostIdAndLikedAccount(Long likedPostId, Account likedAccountId);
+    Optional<Like> findByLikedCommentIdAndLikedAccount(Long likedCommentId, Account likedAccountId);
+    Boolean existsByLikedPostIdAndLikedAccount(Long likedPostId, Account likedAccountId);
+    Boolean existsByLikedCommentIdAndLikedAccount(Long likedCommentId, Account likedAccountId);
 }
