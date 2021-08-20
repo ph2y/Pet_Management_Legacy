58ae3378433f8633b2baab72d6c07a71f3ed4737
Hanbit-Kang <58168528+Hanbit-Kang@users.noreply.github.com>
2021-08-20 15:39:29 +0900

    Union Attachment.kt to FileMetaData.kt

--- client/android/app/src/main/java/com/sju18001/petmanagement/restapi/dao/Attachment.kt
+++
@@ -1,9 +0,0 @@
-package com.sju18001.petmanagement.restapi.dao
-
-data class Attachment(
-    val name: String,
-    val size: Int,
-    val entity: String,
-    val type: String,
-    val url: String
-)
