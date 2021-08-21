e9115747d464dbe88702ed2676f5d980d8496d8a
Komputer-P <wlghks0314@gmail.com>
2021-08-22 00:34:29 +0900

    theme preference implemented

--- client/android/app/src/main/java/com/sju18001/petmanagement/ui/myPage/preferences/ThemePreferencesFragment.kt
+++ client/android/app/src/main/java/com/sju18001/petmanagement/ui/myPage/preferences/ThemePreferencesFragment.kt
@@ -1,12 +1,33 @@
 package com.sju18001.petmanagement.ui.myPage.preferences
 
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.SwitchPreference
 import com.sju18001.petmanagement.R
 
 class ThemePreferencesFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.theme_preferences, rootKey)
+
+        val lightModePreference: Preference? = findPreference("light_theme_preference")
+        lightModePreference?.setOnPreferenceClickListener {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            true
+        }
+
+        val darkModePreference: Preference? = findPreference("dark_theme_preference")
+        darkModePreference?.setOnPreferenceClickListener {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            true
+        }
+
+        val systemModePreference: Preference? = findPreference("system_theme_preference")
+        systemModePreference?.setOnPreferenceClickListener {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+            true
+        }
     }
 }
