b0069bd080d185a86f58335b472b50afe932be6f
Komputer-P <wlghks0314@gmail.com>
2021-08-22 17:46:56 +0900

    save theme preference by sharedPreferences

--- client/android/app/src/main/java/com/sju18001/petmanagement/ui/myPage/preferences/ThemePreferencesFragment.kt
+++ client/android/app/src/main/java/com/sju18001/petmanagement/ui/myPage/preferences/ThemePreferencesFragment.kt
@@ -1,5 +1,6 @@
 package com.sju18001.petmanagement.ui.myPage.preferences
 
+import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.preference.Preference
@@ -12,20 +13,31 @@ class ThemePreferencesFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.theme_preferences, rootKey)
 
+        val sharedPref = activity?.getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE)
+
         val lightModePreference: Preference? = findPreference("light_theme_preference")
         lightModePreference?.setOnPreferenceClickListener {
+            sharedPref?.edit()
+                ?.putInt(getString(R.string.saved_theme_preference_key), AppCompatDelegate.MODE_NIGHT_NO)
+                ?.apply()
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
             true
         }
 
         val darkModePreference: Preference? = findPreference("dark_theme_preference")
         darkModePreference?.setOnPreferenceClickListener {
+            sharedPref?.edit()
+                ?.putInt(getString(R.string.saved_theme_preference_key), AppCompatDelegate.MODE_NIGHT_YES)
+                ?.apply()
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
             true
         }
 
         val systemModePreference: Preference? = findPreference("system_theme_preference")
         systemModePreference?.setOnPreferenceClickListener {
+            sharedPref?.edit()
+                ?.putInt(getString(R.string.saved_theme_preference_key), AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+                ?.apply()
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
             true
         }
