cfca8b11f0bf3ba023a4f38ffe61beac5c19ad65
Komputer-P <wlghks0314@gmail.com>
2021-08-22 17:46:56 +0900

    save theme preference by sharedPreferences

--- client/android/app/src/main/java/com/sju18001/petmanagement/ui/login/LoginActivity.kt
+++ client/android/app/src/main/java/com/sju18001/petmanagement/ui/login/LoginActivity.kt
@@ -1,7 +1,9 @@
 package com.sju18001.petmanagement.ui.login
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.lifecycle.SavedStateViewModelFactory
 import androidx.lifecycle.ViewModelProvider
 import com.sju18001.petmanagement.R
@@ -24,6 +26,13 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // load preference values
+        val sharedPref = getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE)
+        val defaultThemeValue = resources.getInteger(R.integer.saved_default_theme_preference_key)
+        val themeValue = sharedPref.getInt(getString(R.string.saved_theme_preference_key), defaultThemeValue)
+
+        AppCompatDelegate.setDefaultNightMode(themeValue)
+        
         // hide action bar
         supportActionBar?.hide()
 
