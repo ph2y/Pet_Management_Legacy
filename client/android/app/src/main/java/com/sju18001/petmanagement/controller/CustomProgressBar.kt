553a7942836b2e8be892466c89f221dbc942c6c2
Hanbit-Kang <58168528+Hanbit-Kang@users.noreply.github.com>
2021-11-15 20:49:32 +0900

    Add CustomProgressBar to Following, Follower Fragment, Modify CustomProgressBar to add progress bar after view loaded

--- client/android/app/src/main/java/com/sju18001/petmanagement/controller/CustomProgressBar.kt
+++ client/android/app/src/main/java/com/sju18001/petmanagement/controller/CustomProgressBar.kt
@@ -14,30 +14,37 @@ import java.lang.Integer.max
 
 class CustomProgressBar {
     companion object{
-        public fun addProgressBar(context: Context, view: View, size: Int): Int{
-            val linearLayout = LinearLayout(context)
-            linearLayout.layoutParams = view.layoutParams
-            linearLayout.id = View.generateViewId() // id 지정 -> return
-            linearLayout.translationZ = 4f // 맨 앞으로 보내기
-            linearLayout.tag = "CustomProgressBar"
-            (view as ViewGroup).addView(linearLayout)
+        public fun addProgressBar(context: Context, view: View, size: Int){
+            view.post{
+                val linearLayout = LinearLayout(context)
+                linearLayout.layoutParams = view.layoutParams
+                linearLayout.translationZ = 4f // 맨 앞으로 보내기
+                linearLayout.tag = "CustomProgressBar"
+                (view as ViewGroup).addView(linearLayout)
 
-            val progressBar = ProgressBar(context)
-            val params = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, size) // size 지정
-            params.gravity = Gravity.CENTER // 가운데 정렬
-            progressBar.layoutParams = params
-            linearLayout.addView(progressBar)
-
-            return linearLayout.id
+                val progressBar = ProgressBar(context)
+                val params = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, size) // size 지정
+                params.gravity = Gravity.CENTER // 가운데 정렬
+                progressBar.layoutParams = params
+                linearLayout.addView(progressBar)
+            }
         }
 
-        public fun addProgressBar(context: Context, view: View, size: Int, color: Int): Int{
-            val id = addProgressBar(context, view, size)
+        public fun addProgressBar(context: Context, view: View, size: Int, color: Int){
+            view.post{
+                val linearLayout = LinearLayout(context)
+                linearLayout.layoutParams = view.layoutParams
+                linearLayout.translationZ = 4f // 맨 앞으로 보내기
+                linearLayout.setBackgroundColor(context.getColor(color)) // 배경색 지정
+                linearLayout.tag = "CustomProgressBar"
+                (view as ViewGroup).addView(linearLayout)
 
-            val linearLayout = (view as ViewGroup).findViewById<LinearLayout>(id)
-            linearLayout.setBackgroundColor(context.getColor(color)) // 배경색 지정
-
-            return linearLayout.id
+                val progressBar = ProgressBar(context)
+                val params = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, size) // size 지정
+                params.gravity = Gravity.CENTER // 가운데 정렬
+                progressBar.layoutParams = params
+                linearLayout.addView(progressBar)
+            }
         }
 
         public fun removeProgressBar(view: View){
