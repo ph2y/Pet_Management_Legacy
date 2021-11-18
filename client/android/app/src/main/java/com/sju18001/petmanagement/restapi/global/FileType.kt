a9aebd20d19bc2a5552989ced39c927d08dfa545
HanJuK <qwerty03081@gmail.com>
2021-11-18 19:44:15 +0900

    created FileType.kt + modified create/update post layout

---
+++ client/android/app/src/main/java/com/sju18001/petmanagement/restapi/global/FileType.kt
@@ -0,0 +1,8 @@
+package com.sju18001.petmanagement.restapi.global
+
+object FileType {
+    const val GENERAL_FILE = "general"
+    const val IMAGE_FILE = "image"
+    const val VIDEO_FILE = "video"
+    const val AUDIO_FILE = "audio"
+}
