f3e513b0193f9b04d409364789b996d795db9db4
Komputer-P <wlghks0314@gmail.com>
2021-11-20 00:04:09 +0900

    Pessimistic Lock added to resolve transaction conflict

--- server/src/main/java/com/sju18/petmanagement/domain/community/post/dao/PostRepository.java
+++ server/src/main/java/com/sju18/petmanagement/domain/community/post/dao/PostRepository.java
@@ -4,16 +4,20 @@ import com.sju18.petmanagement.domain.account.dao.Account;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.LockModeType;
 import java.util.Collection;
 import java.util.Optional;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
     Optional<Post> findById(Long id);
+
+    @Lock(LockModeType.PESSIMISTIC_READ)
     Optional<Post> findByAuthorAndId(Account author, Long id);
 
     @Query(
