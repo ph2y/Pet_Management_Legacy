c6a6846d7b1d995586d5bcbe6dd6d1de4889d897
HanJuK <qwerty03081@gmail.com>
2021-11-20 14:47:59 +0900

    basic layout for post general files

---
+++ client/android/app/src/main/java/com/sju18001/petmanagement/ui/community/post/PostGeneralFilesActivity.kt
@@ -0,0 +1,12 @@
+package com.sju18001.petmanagement.ui.community.post
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sju18001.petmanagement.R
+
+class PostGeneralFilesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_post_general_files)
+    }
+}
