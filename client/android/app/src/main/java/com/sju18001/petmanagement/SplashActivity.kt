bae72766cacc01fb2e45a12018028de32780a916
Komputer-P <wlghks0314@gmail.com>
2021-11-23 17:50:08 +0900

    logic refactored

--- client/android/app/src/main/java/com/sju18001/petmanagement/SplashActivity.kt
+++ client/android/app/src/main/java/com/sju18001/petmanagement/SplashActivity.kt
@@ -29,41 +29,41 @@ class SplashActivity: AppCompatActivity() {
         AppCompatDelegate.setDefaultNightMode(themeValue)
 
         // token validation check & auto login
-        if (SessionManager.fetchUserToken(applicationContext) != null) {
-            validateTokenAndLogin(SessionManager.fetchUserToken(applicationContext)!!)
-        }
-        else { // token not valid -> LoginActivity
-            startLoginActivityAndFinish()
-        }
+        validateTokenAndLogin()
     }
 
-    private fun validateTokenAndLogin(token: String) {
-        val call = RetrofitBuilder.getServerApiWithToken(token).fetchAccountReq(ServerUtil.getEmptyBody())
-        call.enqueue(object: Callback<FetchAccountResDto> {
-            override fun onResponse(
-                call: Call<FetchAccountResDto>,
-                response: Response<FetchAccountResDto>
-            ) {
-               if(response.isSuccessful) { // token is valid -> MainActivity
-                   val intent = Intent(this@SplashActivity, MainActivity::class.java)
-                   response.body()?.run{
-                       val account = Account(id, username, email, phone, null, marketing, nickname, photoUrl, userMessage, representativePetId)
-                       SessionManager.saveLoggedInAccount(this@SplashActivity, account)
+    private fun validateTokenAndLogin() {
+        if (SessionManager.fetchUserToken(applicationContext) != null) {
+            val call = RetrofitBuilder.getServerApiWithToken(SessionManager.fetchUserToken(applicationContext)!!).fetchAccountReq(ServerUtil.getEmptyBody())
+            call.enqueue(object: Callback<FetchAccountResDto> {
+                override fun onResponse(
+                    call: Call<FetchAccountResDto>,
+                    response: Response<FetchAccountResDto>
+                ) {
+                    if(response.isSuccessful) { // token is valid -> MainActivity
+                        val intent = Intent(this@SplashActivity, MainActivity::class.java)
+                        response.body()?.run{
+                            val account = Account(id, username, email, phone, null, marketing, nickname, photoUrl, userMessage, representativePetId)
+                            SessionManager.saveLoggedInAccount(this@SplashActivity, account)
 
-                       startActivity(intent)
-                       finish()
-                   }
-               }
-               else { // token not valid -> LoginActivity
-                   startLoginActivityAndFinish()
-               }
-            }
+                            startActivity(intent)
+                            finish()
+                        }
+                    }
+                    else { // token not valid -> LoginActivity
+                        startLoginActivityAndFinish()
+                    }
+                }
 
-            override fun onFailure(call: Call<FetchAccountResDto>, t: Throwable) {
-                Toast.makeText(this@SplashActivity, t.message.toString(), Toast.LENGTH_LONG).show()
-                Log.d("error", t.message.toString())
-            }
-        })
+                override fun onFailure(call: Call<FetchAccountResDto>, t: Throwable) {
+                    Toast.makeText(this@SplashActivity, t.message.toString(), Toast.LENGTH_LONG).show()
+                    Log.d("error", t.message.toString())
+                }
+            })
+        }
+        else { // token is empty -> LoginActivity
+            startLoginActivityAndFinish()
+        }
     }
 
     private fun startLoginActivityAndFinish() {
