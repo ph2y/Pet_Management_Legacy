7da322ef013f6758ce239eb75aac409243629c80
newcentury99 <newcentury99@newcentury99ui-MacBookPro.local>
2022-01-06 12:34:30 +0900

    Create new iOS app project

---
+++ client/ios/pet_management/pet_management/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pet_management
+//
+//  Created by newcentury99 on 2022/01/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ client/ios/pet_management/pet_managementUITests/pet_managementUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  pet_managementUITestsLaunchTests.swift
+//  pet_managementUITests
+//
+//  Created by newcentury99 on 2022/01/06.
+//
+
+import XCTest
+
+class pet_managementUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
